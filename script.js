c190d294fbb3ba271a0349feb8a8e99472baf30b
jamshedkhan319 <jamshedkhanbeg391@gmail.com>
2024-12-17 00:43:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,23 @@ const videos = [
 
 
     {
-        src: "test.mp4",
+        src: "arabic1.mp4",
         title: "Video 00003",
         description: "This is ption of Video 3."
+    },
+
+
+    {
+        src: "arabic1.mp4",
+        title: "Video 3",
+        description: "This is the description of Video 3."
+    },
+
+    {
+        src: "arabic1.mp4",
+        title: "Video 3",
+        description: "This is the description of Video 3."
+
     }
 
     
